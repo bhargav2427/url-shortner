c7f0a5f8ababfbf2fdedf60e9a76c1b2777f3439
bhargav2427 <akhani.bharga@gmail.com>
2020-09-18 08:22:00 +0530

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+# os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = DATABASE_URL = 'sqlite:///db.sqlite3'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
